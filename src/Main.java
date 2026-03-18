dcff69b95ca62336332d8b327c8ad9a5338c85b6
sunaad7 <sunaadjoel@gmail.com>
2026-03-18 14:27:44 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to Hotel Booking System!");
+
+        // Display application name and version
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+
+        // Indicate application termination
+        System.out.println("Application started successfully and will now exit.");
+    }
+}
